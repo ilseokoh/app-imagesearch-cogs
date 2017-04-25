4d48caf09099c14c9fbf75da84907060da382490
오일석 <onestone@live.co.kr>
2017-04-25 21:58:54 +0900

    add keys

--- ImageSearch/ImageSearch/Services/ServiceKeys.cs
+++ ImageSearch/ImageSearch/Services/ServiceKeys.cs
@@ -10,10 +10,10 @@ namespace ImageSearch.Services
     public class CognitiveServicesKeys
     {
         //Setup a Emotion API key for Cognitive Services at: https://www.microsoft.com/cognitive-services/
-        public const string Emotion = "__ENTER YOUR KEY__";
+        public const string Emotion = "4efacb9e71fd4a458457f179891be502";
 
-		//Setup a Bing Search API key from: http://www.microsoft.com/cognitive-services
-		public const string BingSearch = "__ENTER YOUR KEY__";
+        //Setup a Bing Search API key from: http://www.microsoft.com/cognitive-services
+        public const string BingSearch = "54317f6200924e69a1d8999753cc978b";
 
-	} 
+    } 
 }
